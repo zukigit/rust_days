51931ff2a975af0ec6c6cf3c92f56bf3226f363b
zuki <zukidono@gmail.com>
2026-06-15 22:56:00 +0700

    added day9 and exercise1

---
+++ day9/src/main.rs
@@ -0,0 +1,58 @@
+trait Animal {
+    fn name(&self) -> String;
+
+    // default function
+    fn speak(&self) -> String {
+        String::from("... makes a sound")
+    }
+}
+
+struct Dog {
+    name: String
+}
+
+struct Cat {
+    name: String
+}
+
+impl Animal for Dog {
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    fn speak(&self) -> String {
+        format!("{} says Woof!", self.name())
+    }
+}
+
+impl Dog {
+    fn new(name: &str) -> Self {
+        Dog { name: name.to_string() }
+    }
+}
+
+impl Cat {
+    fn new(name: &str) -> Self {
+        Cat { name: name.to_string() }
+    }
+}
+
+impl Animal for Cat {
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    fn speak(&self) -> String {
+        format!("{} says Meow!", self.name())
+    }
+}
+
+fn main() {
+    // Day 10: Traits 🦀
+
+    // 1
+    let dog = Dog::new("Buddy");
+    let cat = Cat::new("Whiskers");
+    println!("{}", dog.speak()); // Buddy says Woof!
+    println!("{}", cat.speak()); // Whiskers says Meow!
+}
